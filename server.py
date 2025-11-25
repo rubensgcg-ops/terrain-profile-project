ec25015b9a76f8727505c1e205fc6afc497e7bbe
Rubens Godoi <rubens.gcg@gmail.com>
2025-11-25 16:54:16 -0300

    alterado o app.run

--- server.py
+++ server.py
@@ -173,4 +173,6 @@ def profile_csv():
 # =======================================
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=False)
+   #app.run(host="0.0.0.0", port=8000, debug=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
